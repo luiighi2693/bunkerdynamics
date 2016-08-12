321f157442b1dd46d515e41e20f5ab4cb607de81
luiighi2693 <luiscarvajal2693@gmail.com>
2016-08-11 21:54:43 -0400

    integracion de imagenes faltantes 2

--- mapaVenezuela.php
+++ mapaVenezuela.php
@@ -55,128 +55,128 @@ fclose($fp);
 <div style="display: none;">
     <div id="myContentMaracaibo">
         <ul>
-            <li><a href="images/maracaiboMin.png">Bunkers</a></li>
-            <li><a href="images/tablaMaracaibo.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/maracaiboMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaMaracaibo<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentBajoGrande">
         <ul>
-            <li><a href="images/BajoGrandeMin.png">Bunkers</a></li>
-            <li><a href="images/tablaBajoGrande.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/BajoGrandeMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaBajoGrande<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentLaSalina">
         <ul>
-            <li><a href="images/LaSalinaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaLaSalina.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/LaSalinaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaLaSalina<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentElTablazo">
         <ul>
-            <li><a href="images/ElTablazoMin.png">Bunkers</a></li>
-            <li><a href="images/tablaElTablazo.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/ElTablazoMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaElTablazo<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPtoMiranda">
         <ul>
-            <li><a href="images/PtoMirandaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPtoMiranda.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/PtoMirandaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPtoMiranda<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentCardon">
         <ul>
-            <li><a href="images/CardonMin.png">Bunkers</a></li>
-            <li><a href="images/tablaCardon.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/cardonMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaCardon<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentGuaranao">
         <ul>
-            <li><a href="images/GuaranaoMin.png">Bunkers</a></li>
-            <li><a href="images/tablaGuaranao.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/guaranaoMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaGuaranao<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentAmuay">
         <ul>
-            <li><a href="images/AmuayMin.png">Bunkers</a></li>
-            <li><a href="images/tablaAmuay.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/amuayMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaAmuay<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentElPalito">
         <ul>
-            <li><a href="images/ElPalitoMin.png">Bunkers</a></li>
-            <li><a href="images/tablaElPalito.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/elPalitoMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaElPalito<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPuertoCabello">
         <ul>
-            <li><a href="images/PuertoCabelloMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPuertoCabello.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/puertoCabelloMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPuertoCabello<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentLaGuaira">
         <ul>
-            <li><a href="images/laGuairaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaLaGuaira.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/laGuairaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaLaGuaira<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentJoseTerminal">
         <ul>
-            <li><a href="images/joseTerminalMin.png">Bunkers</a></li>
-            <li><a href="images/tablaJoseTerminal.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/joseTerminalMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaJoseTerminal<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentGuanta">
         <ul>
-            <li><a href="images/guantaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaGuanta.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/guantaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaGuanta<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPuertoLaCruz">
         <ul>
-            <li><a href="images/puertoLaCruzMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPuertoLaCruz.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/puertoLaCruzMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPuertoLaCruz<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPertigalete">
         <ul>
-            <li><a href="images/pertigaleteMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPertigalete.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/pertigaleteMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPertigalete<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentCumana">
         <ul>
-            <li><a href="images/cumanaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaCumana.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/cumanaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaCumana<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentGuiria">
         <ul>
-            <li><a href="images/guiriaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaGuiria.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/guiriaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaGuiria<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentMatanzas">
         <ul>
-            <li><a href="images/matanzasMin.png">Bunkers</a></li>
-            <li><a href="images/tablaMatanzas.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/matanzasMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaMatanzas<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPuertoOrdaz">
         <ul>
-            <li><a href="images/puertoOrdazMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPuertoOrdaz.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/puertoOrdazMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPuertoOrdaz<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentPalua">
         <ul>
-            <li><a href="images/paluaMin.png">Bunkers</a></li>
-            <li><a href="images/tablaPalua.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/paluaMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaPalua<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
     <div id="myContentBocaGrande">
         <ul>
-            <li><a href="images/bocaGrandeMin.png">Bunkers</a></li>
-            <li><a href="images/tablaBocaGrande.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
+            <li><a href="images/bocaGrandeMin<?php if($leng=="En"){echo "En";}?>.png">Bunkers</a></li>
+            <li><a href="images/tablaBocaGrande<?php if($leng=="En"){echo "En";}?>.jpg"  ><?php if($leng=="Es"){echo "Lubricantes Marinos";}else{echo "Marine Lubricants";}?></a></li>
         </ul>
     </div>
 </div>
